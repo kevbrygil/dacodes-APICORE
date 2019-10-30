c2c70122a4ab4b0d18e1f381eb371f4dae3a4863
gilnextor <tamayo_144@hotmail.com>
2019-10-30 15:55:29 -0600

    cambios para docker

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM mcr.microsoft.com/dotnet/core/sdk:3.0 AS build
 EXPOSE 5002
+EXPOSE 5432
+EXPOSE 80
 
 WORKDIR /src
 COPY dacodes-APICORE.csproj .
--- dbscripts/migrate_1.sql
+++ dbscripts/migrate_1.sql
@@ -1,6 +1,4 @@
-﻿\connect dacodes_db
-
-CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
+﻿CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
     "MigrationId" character varying(150) NOT NULL,
     "ProductVersion" character varying(32) NOT NULL,
     CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY ("MigrationId")
